694f6d28cd6414db1307c8ee295a551af45e417a
prosku <proskeu@student.ethz.ch>
2021-08-19 17:10:58 +0200

    Remove cartopy to facilitate checks and add to workflow

--- examples/integration_example.py
+++ examples/integration_example.py
@@ -14,15 +14,6 @@ matplotlib.use('Agg') # https://stackoverflow.com/questions/37604289/tkinter-tcl
 import matplotlib.pyplot as plt
 
 from netCDF4 import Dataset
-import os
-#import fnmatch
-#import pandas as pd
-
-#Plotting
-from cartopy import config
-import cartopy.crs as ccrs
-import cartopy.feature as cfeature
-from cartopy.util import add_cyclic_point
 
 #PySphereX
 from pyspherex import Expansion
@@ -37,9 +28,7 @@ def plot_sph_harm(var, unit, data):
     data=data[var][0,:,:]
     result = Expansion.from_data(phi, theta, data, degree_max)
 
-    # make it see the data as continuous
-    data, lon = add_cyclic_point(data, coord=lons)
-    data_sh, lon = add_cyclic_point(np.real(result(phi, theta)), coord=lons)
+    data_sh = np.real(result(phi, theta))
 
     fig = plt.figure(figsize=(3.27,3.27*2))
     plt.subplots_adjust(0,0,1,1)
@@ -47,53 +36,28 @@ def plot_sph_harm(var, unit, data):
     vmin=np.nanmin([data,data_sh])
     vmax=np.nanmax([data,data_sh])
     vext = np.nanmax([np.abs(vmin), np.abs(vmax)])/2 # make colors stronger
-    ax1 = fig.add_subplot(311, projection=ccrs.PlateCarree())
+    ax1 = fig.add_subplot(311)
     # https://stackoverflow.com/questions/33942233/how-do-i-change-matplotlibs-subplot-projection-of-an-existing-axis
 
-    print('fine')
-
     if not np.any(data < 0):
-        im = ax1.pcolormesh(lon, lats, data, cmap='RdBu_r', norm=matplotlib.colors.LogNorm(vmin=np.nanmin(data), vmax=np.nanmax(data)))
+        im = ax1.pcolormesh(lons, lats, data, cmap='RdBu_r', norm=matplotlib.colors.LogNorm(vmin=np.nanmin(data), vmax=np.nanmax(data)))
     else:
-        im = ax1.pcolormesh(lon, lats, data, cmap='RdBu_r', vmin=-vext, vmax=vext)
-    print('fine')
-    ax1.add_feature(cfeature.NaturalEarthFeature('physical', 'land', '50m', edgecolor='k', facecolor='none'))
-    print('fine')
-    ax1.set_xlabel('lon')
-    ax1.set_ylabel('lat')
+        im = ax1.pcolormesh(lons, lats, data, cmap='RdBu_r', vmin=-vext, vmax=vext)
     # axis labels
-    print('fine')
-    gl1 = ax1.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, alpha=0.5)
-    print('fine')
-    gl1.xlabels_top = False
-    gl1.ylabels_right = False
-    gl1.xlabels_bottom = False
-    gl1.ylabels_left = False
-    ax1.set_xlabel(r'Longitude ($^\circ$E)')
-    ax1.set_ylabel(r'Latitude ($^\circ$N)')
+    ax1.set_xlabel(r'Longitude')
+    ax1.set_ylabel(r'Latitude')
     ax1.set_yticks([-60,-30,0,30,60])
-    ax1.set_xticks([-120,-60,0,60,120])
 
-    ax2 = fig.add_subplot(312, projection=ccrs.PlateCarree())#axs[1]
+    ax2 = fig.add_subplot(312)
     if not np.any(data < 0):
-        im = ax2.pcolormesh(lon, lats, data_sh, cmap='RdBu_r', norm=matplotlib.colors.LogNorm(vmin=np.nanmin(data), vmax=np.nanmax(data)))
+        im = ax2.pcolormesh(lons, lats, data_sh, cmap='RdBu_r', norm=matplotlib.colors.LogNorm(vmin=np.nanmin(data), vmax=np.nanmax(data)))
     else:
-        im = ax2.pcolormesh(lon, lats, data_sh, cmap='RdBu_r', vmin=-vext, vmax=vext)
-    ax2.set_xlabel('lon')
-    ax2.set_ylabel('lat')
-    ax2.add_feature(cfeature.NaturalEarthFeature('physical', 'land', '50m', edgecolor='k', facecolor='none'))
+        im = ax2.pcolormesh(lons, lats, data_sh, cmap='RdBu_r', vmin=-vext, vmax=vext)
 
     # axis labels
-    gl = ax2.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, alpha=0.5)
-    gl.xlabels_top = False
-    gl.ylabels_right = False
-    gl.xlabels_bottom = False
-    gl.ylabels_left = False
-
-    ax2.set_xlabel(r'Longitude ($^\circ$E)')
-    ax2.set_ylabel(r'Latitude ($^\circ$N)')
+    ax2.set_xlabel(r'Longitude')
+    ax2.set_ylabel(r'Latitude')
     ax2.set_yticks([-60,-30,0,30,60])
-    ax2.set_xticks([-120,-60,0,60,120])
     fig.colorbar(im, ax=[ax1, ax2], label=var+' ('+unit+')', shrink=0.8, anchor=(2.5,0.5))
 
     ax1.set_title('Data')
@@ -102,10 +66,7 @@ def plot_sph_harm(var, unit, data):
     spec2 = np.zeros((len(result.coeffs)))
     for l in result.coeffs:
         spec2[l] = np.abs(result.coeffs[l][l])**2 / 4 / np.pi
-    #axs[2].remove()
     ax = fig.add_subplot(313)
-    #ax = fig.add_subplot(2,2,(3,4))
-    # https://scientificallysound.org/2017/09/07/matplotlib-subplots/
     ax.plot(result.spectrum[0], result.spectrum[1]**(1/2), label='all $m$', color='black')
     ax.plot(result.spectrum[0], spec2**(1/2), label='$m=0$ only', color='grey')
     ax.set_yscale('log')
@@ -114,7 +75,7 @@ def plot_sph_harm(var, unit, data):
     ax.legend()
 
     # Set spacing between plots
-    plt.subplots_adjust(wspace=0.6)
+    plt.subplots_adjust(hspace=0.5)
     
     plt.savefig('geogr_'+var+'.pdf', bbox_inches='tight')
     plt.close()
